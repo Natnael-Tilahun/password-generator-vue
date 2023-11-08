f2c534de9cb074d785a40950697db11178f0e39c
natnael-tilahun <natnaeltilahun97@gmail.com>
2023-11-09 01:21:50 +0300

    Edit sidebar layout

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="flex flex-col md:w-1/2 justify-center items-center lg:h-full md:gap-5 gap-2 px-5 py-10 text-center lg:p-14"
+    class="flex flex-col md:w-1/2 justify-center items-center md:gap-5 gap-2 px-5 py-10 text-center lg:p-14"
   >
     <h1 class="md:text-3xl text-2xl font-bold tracking-wide text-white">
       Generate a <span class="text-emerald-400">random password</span>
